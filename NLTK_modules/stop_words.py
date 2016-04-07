9b2de6a271aea457a0b326c3bf817cc58ef8161f
Maverick <maverick@Palashs-MacBook-Pro.local>
2016-04-07 12:36:58 -0400

    Stop words module completed

--- NLTK_modules/stop_words.py
+++ NLTK_modules/stop_words.py
@@ -4,4 +4,15 @@ from nltk.tokenize import word_tokenize
 example_sentence = "This is an example showing stop word filtration"
 
 stop_words = set(stopwords.words("english"))
-print(stop_words)
+#print(stop_words)
+
+words = word_tokenize(example_sentence)
+
+filtered_sentence = []
+
+for w in words:
+	if w not in stop_words:
+		filtered_sentence.append(w)
+
+print(example_sentence)
+print(filtered_sentence)
