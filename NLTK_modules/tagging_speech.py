c52b34457a9ac2a8f9471e8712f5f55ebf20b10b
PalashMatey <pm2824@columbia.edu>
2016-04-07 17:15:18 -0400

    Chunking implemented

--- NLTK_modules/tagging_speech.py
+++ NLTK_modules/tagging_speech.py
@@ -1,7 +1,9 @@
 import nltk
 from nltk.corpus import state_union
 from nltk.tokenize import PunktSentenceTokenizer
-
+'''
+Also included is chunking, so we can collect and store as chunks the proper nouns
+'''
 
 train_text = state_union.raw('2005-GWBush.txt')
 sample_text = state_union.raw('2006-GWBush.txt')
@@ -10,13 +12,20 @@ sample_text = state_union.raw('2006-GWBush.txt')
 custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
 
 tokenized = custom_sent_tokenizer.tokenize(sample_text)
-
+#Adding chunking, groups of nouns
+#This was we can maybe get the count of all the nouns. 
+#That is maybe we can use this to chunk all the people other people mention in 
+#DonaldTrump reddits(for example)
 def process_content():
 	try:
 		for i in tokenized:
 			words = nltk.word_tokenize(i)
 			tagged = nltk.pos_tag(words)
-			print(tagged)
+			chunkGram = r"""Chunk: {<NNP>} """
+			chunkParser = nltk.RegexpParser(chunkGram)
+			chunked = chunkParser.parse(tagged)
+#			print(chunked)
+			chunked.draw()
 
 	except Exception as e:
 		print(str(e))
