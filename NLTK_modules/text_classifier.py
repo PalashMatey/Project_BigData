dcba84f063313ed741076f50ed84507cada56369
PalashMatey <pm2824@columbia.edu>
2016-04-09 23:23:39 -0400

    basic text classification

---
+++ NLTK_modules/text_classifier.py
@@ -0,0 +1,28 @@
+'''
+Can be used for a variety of things. Sentiment or meaning, as a form of opinion mining
+Pos or neg as sentiment analysis
+'''
+import nltk
+import random
+from nltk.corpus import movie_reviews #these movie reviews are already labelled
+
+documents = []
+
+for category in movie_reviews.categories():
+	for fileid in movie_reviews.fileids(category):
+		documents.append((list(movie_reviews.words(fileid)),category))
+
+#list method in python converts tuples to lists
+random.shuffle(documents)
+#print(documents[1])
+
+all_words = []
+for w in movie_reviews.words():
+	all_words.append(w.lower())
+
+all_words = nltk.FreqDist(all_words)
+#print(all_words.most_common(15))
+
+#print(all_words['stupid'])
+
+
