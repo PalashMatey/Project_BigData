a1c14955adf42a1cc82ca9a91f07f52fec2f1c4b
PalashMatey <pm2824@columbia.edu>
2016-04-17 01:36:17 -0400

    Naive Bayes Algo implemented Accuracy 72%

--- NLTK_modules/text_classifier.py
+++ NLTK_modules/text_classifier.py
@@ -25,4 +25,25 @@ all_words = nltk.FreqDist(all_words)
 
 #print(all_words['stupid'])
 
+word_features = list(all_words.keys())[:3000]
 
+def find_features(document):
+	words = set(document)
+	features = {}
+	for w in word_features:
+		features[w] = (w in words)
+	return features
+
+print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
+featuresets = [(find_features(rev),category) for 
+(rev,category) in documents]
+
+# set that we'll train our classifier with
+training_set = featuresets[:1900]
+
+# set that we'll test against.
+testing_set = featuresets[1900:]
+
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
+classifier.show_most_informative_features(15)
