935b972723d871bef0e7c9a044b04838c9c8f80a
PalashMatey <pm2824@columbia.edu>
2016-05-07 23:34:41 -0400

    Voting

--- NLTK_modules/text_classifier.py
+++ NLTK_modules/text_classifier.py
@@ -10,6 +10,28 @@ from nltk.classify.scikitlearn import SklearnClassifier
 from sklearn.linear_model import LogisticRegression,SGDClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
 from sklearn.naive_bayes import MultinomialNB,GaussianNB, BernoulliNB
+from nltk.classify import ClassifierI
+from statistics import mode
+
+class VoteClassifier(ClassifierI):
+	def __init__(self, *classifiers):
+		self._classifiers = classifiers
+	def classify(self,features):
+		votes = []
+		for c in self._classifiers:
+			v = c.classify(features)
+			votes.append(v)
+		return mode(votes)
+	def confidence(self,features):
+		votes = []
+		for c in self._classifiers:
+			v = c.classify(features)
+			votes.append(v)
+		choice_votes = votes.count(mode(votes))
+		conf = choice_votes / len(votes)
+		return conf
+		
+
 
 documents = []
 
@@ -63,24 +85,23 @@ classifier.show_most_informative_features(15)
 
 MNB_classifier = SklearnClassifier(MultinomialNB())
 MNB_classifier.train(training_set)
-print("MultinomialNB accuracy percent:",nltk.classify.accuracy(MNB_classifier, testing_set))
+print("MultinomialNB accuracy percent:",(nltk.classify.accuracy(MNB_classifier, testing_set))*100)
 
 BNB_classifier = SklearnClassifier(BernoulliNB())
 BNB_classifier.train(training_set)
-print("BernoulliNB accuracy percent:",nltk.classify.accuracy(BNB_classifier, testing_set))
+print("BernoulliNB accuracy percent:",(nltk.classify.accuracy(BNB_classifier, testing_set))*100)
 
 LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
 LogisticRegression_classifier.train(training_set)
-print("LogisticRegression_classifier accuracy percent:", 
-(nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
 
 SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
 SGDClassifier_classifier.train(training_set)
 print("SGDClassifier_classifier accuracy percent:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
 
-SVC_classifier = SklearnClassifier(SVC())
-SVC_classifier.train(training_set)
-print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
+#SVC_classifier = SklearnClassifier(SVC())
+#SVC_classifier.train(training_set)
+#print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
 
 LinearSVC_classifier = SklearnClassifier(LinearSVC())
 LinearSVC_classifier.train(training_set)
@@ -89,3 +110,14 @@ print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSV
 NuSVC_classifier = SklearnClassifier(NuSVC())
 NuSVC_classifier.train(training_set)
 print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
+
+voted_classifier = VoteClassifier(classifier,MNB_classifier,BNB_classifier,LogisticRegression_classifier,SGDClassifier_classifier,LinearSVC_classifier,NuSVC_classifier)
+
+print("Voted classifier accuracy percent:", (nltk.classify.accuracy(voted_classifier, testing_set))*100)
+
+print("Classification:", voted_classifier.classify(testing_set[0][0]),"Confidence %:", (voted_classifier.confidence(testing_set[0][0]))*100)
+print("Classification:", voted_classifier.classify(testing_set[1][0]),"Confidence %:", (voted_classifier.confidence(testing_set[1][0]))*100)
+print("Classification:", voted_classifier.classify(testing_set[2][0]),"Confidence %:", (voted_classifier.confidence(testing_set[2][0]))*100)
+print("Classification:", voted_classifier.classify(testing_set[3][0]),"Confidence %:", (voted_classifier.confidence(testing_set[3][0]))*100)
+print("Classification:", voted_classifier.classify(testing_set[4][0]),"Confidence %:", (voted_classifier.confidence(testing_set[4][0]))*100)
+print("Classification:", voted_classifier.classify(testing_set[5][0]),"Confidence %:", (voted_classifier.confidence(testing_set[5][0]))*100)
